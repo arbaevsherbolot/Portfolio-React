6fef355c5166176ab883a9f036f4ea1abf11dae9
Sherbolot <arbaevsherbolot1@gmail.com>
2023-04-13 16:41:39 +0600

    added burger menu

--- src/components/wrapper/header/Header.jsx
+++ src/components/wrapper/header/Header.jsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 import { Resume } from "./header_components/resume/Resume";
 import logo from "../../../assets/logo.webp";
 import styles from "./Header.module.scss";
 
 export const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <>
       <div className={styles.navbar}>
@@ -12,8 +13,9 @@ export const Header = () => {
           <img className={styles.logo} src={logo} alt="logo" />
         </Link>
         <div className={styles.nav_right}>
-          <div className={styles.links}>
+          <div className={isOpen ? styles.links_open : styles.links}>
             <NavLink
+              onClick={() => setIsOpen(!isOpen)}
               to="/"
               className={({ isActive }) =>
                 isActive ? styles.active_link : styles.link
@@ -22,6 +24,7 @@ export const Header = () => {
             </NavLink>
 
             <NavLink
+              onClick={() => setIsOpen(!isOpen)}
               to="/about"
               className={({ isActive }) =>
                 isActive ? styles.active_link : styles.link
@@ -30,6 +33,7 @@ export const Header = () => {
             </NavLink>
 
             <NavLink
+              onClick={() => setIsOpen(!isOpen)}
               to="/contact"
               className={({ isActive }) =>
                 isActive ? styles.active_link : styles.link
@@ -38,6 +42,7 @@ export const Header = () => {
             </NavLink>
 
             <a
+              onClick={() => setIsOpen(!isOpen)}
               className={styles.link}
               href="https://github.com/Sherbo1ot"
               target="_blank">
@@ -47,7 +52,11 @@ export const Header = () => {
           <Resume />
         </div>
 
-        <div className={styles.burger_menu}></div>
+        <div className={styles.burger_menu} onClick={() => setIsOpen(!isOpen)}>
+          <span className={isOpen ? styles.open : ""} />
+          <span className={isOpen ? styles.open : ""} />
+          <span className={isOpen ? styles.open : ""} />
+        </div>
       </div>
     </>
   );
