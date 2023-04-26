6d0500b0db54a2b0b228398d00408b4e018ac036
Sherbolot <arbaevsherbolot1@gmail.com>
2023-04-27 01:41:29 +0600

    Change a home page 🙈

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import TypeWriterEffect from "react-typewriter-effect";
 import scss from "./Home.module.scss";
 
 export const Home = () => {
+  const text = [
+    "Sherbolot Arbaev 👋🏻",
+    "Full-Stack developer 🧑🏻‍💻",
+    "Software Engineer 😎",
+    "Graphic designer 🧑🏻‍🎨",
+    "UI-UX designer 🧑🏻‍🎨",
+    "Student 👨🏻‍🎓",
+  ];
   return (
     <>
       <div className={scss.page_wrapper}>
@@ -9,9 +18,26 @@ export const Home = () => {
           <div className={scss.box}>
             <div className={scss.box_title}>
               <h3>
-                Hello, I'm Sherbolot, a Full-Stack developer with 3 years of
-                experience.
+                Hey I'm
+                <TypeWriterEffect
+                  textStyle={{
+                    color: "rgb(85, 73, 255)",
+                    fontWeight: "700",
+                    fontSize: "3.3rem",
+                  }}
+                  cursorColor="#f6f6f6"
+                  startDelay={200}
+                  multiText={text}
+                  multiTextDealy={1000}
+                  typeSpeed={50}
+                  multiTextLoop
+                />{" "}
               </h3>
+              <p>
+                I develop the user interface, Web page development, I create UX
+                element interactions, Design and mockups of products for
+                companies.
+              </p>
             </div>
           </div>
           <div className={scss.box}></div>
