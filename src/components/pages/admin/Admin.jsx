c67196881c0145fb321a04628c9fab414124773a
Sherbolot <arbaevsherbolot1@gmail.com>
2023-05-24 15:30:11 +0600

    Added posts

--- src/components/pages/admin/Admin.jsx
+++ src/components/pages/admin/Admin.jsx
@@ -2,6 +2,8 @@ import { useEffect } from "react";
 import { useAuthUser } from "react-auth-kit";
 import { useSignOut } from "react-auth-kit";
 import { useNavigate } from "react-router-dom";
+import icon from "../../../assets/ios_programmer.png";
+import post_img from "../../../assets/full-stack.jpg";
 import styles from "./Admin.module.scss";
 
 export const Admin = () => {
@@ -32,12 +34,10 @@ export const Admin = () => {
     <>
       {auth() ? (
         <div className="page_wrapper">
+          <h3 className={styles.title_page}>Admin Page</h3>
+
           <div className={styles.profile}>
-            <img
-              src="https://support.signal.org/hc/article_attachments/360083910451/animated-2.gif"
-              alt="Icon"
-              className={styles.user_img}
-            />
+            <img src={icon} alt="Icon" className={styles.user_img} />
 
             <div className={styles.user_data}>
               <h1 className={styles.username}>{auth().username}</h1>
@@ -49,6 +49,110 @@ export const Admin = () => {
             <button className={styles.btn} onClick={HandleSignOut}>
               Sign Out
             </button>
+          </div>
+
+          <div className={styles.content}>
+            <div className={styles.post}>
+              <img src={post_img} alt="Post-Image" className={styles.img} />
+
+              <div className={styles.text}>
+                <span className={styles.date}>05.24.2023</span>
+                <h3 className={styles.title}>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </h3>
+                <p className={styles.desc}>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Aliquam eos inventore vel distinctio nesciunt asperiores
+                  dolore nemo eveniet iusto nihil laborum velit amet magni,
+                  nostrum totam cupiditate accusamus similique eius?
+                </p>
+              </div>
+            </div>
+
+            <div className={styles.post}>
+              <img src={post_img} alt="Post-Image" className={styles.img} />
+
+              <div className={styles.text}>
+                <span className={styles.date}>05.24.2023</span>
+                <h3 className={styles.title}>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </h3>
+                <p className={styles.desc}>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Aliquam eos inventore vel distinctio nesciunt asperiores
+                  dolore nemo eveniet iusto nihil laborum velit amet magni,
+                  nostrum totam cupiditate accusamus similique eius?
+                </p>
+              </div>
+            </div>
+
+            <div className={styles.post}>
+              <img src={post_img} alt="Post-Image" className={styles.img} />
+
+              <div className={styles.text}>
+                <span className={styles.date}>05.24.2023</span>
+                <h3 className={styles.title}>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </h3>
+                <p className={styles.desc}>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Aliquam eos inventore vel distinctio nesciunt asperiores
+                  dolore nemo eveniet iusto nihil laborum velit amet magni,
+                  nostrum totam cupiditate accusamus similique eius?
+                </p>
+              </div>
+            </div>
+
+            <div className={styles.post}>
+              <img src={post_img} alt="Post-Image" className={styles.img} />
+
+              <div className={styles.text}>
+                <span className={styles.date}>05.24.2023</span>
+                <h3 className={styles.title}>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </h3>
+                <p className={styles.desc}>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Aliquam eos inventore vel distinctio nesciunt asperiores
+                  dolore nemo eveniet iusto nihil laborum velit amet magni,
+                  nostrum totam cupiditate accusamus similique eius?
+                </p>
+              </div>
+            </div>
+
+            <div className={styles.post}>
+              <img src={post_img} alt="Post-Image" className={styles.img} />
+
+              <div className={styles.text}>
+                <span className={styles.date}>05.24.2023</span>
+                <h3 className={styles.title}>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </h3>
+                <p className={styles.desc}>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Aliquam eos inventore vel distinctio nesciunt asperiores
+                  dolore nemo eveniet iusto nihil laborum velit amet magni,
+                  nostrum totam cupiditate accusamus similique eius?
+                </p>
+              </div>
+            </div>
+
+            <div className={styles.post}>
+              <img src={post_img} alt="Post-Image" className={styles.img} />
+
+              <div className={styles.text}>
+                <span className={styles.date}>05.24.2023</span>
+                <h3 className={styles.title}>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </h3>
+                <p className={styles.desc}>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Aliquam eos inventore vel distinctio nesciunt asperiores
+                  dolore nemo eveniet iusto nihil laborum velit amet magni,
+                  nostrum totam cupiditate accusamus similique eius?
+                </p>
+              </div>
+            </div>
           </div>
         </div>
       ) : null}
