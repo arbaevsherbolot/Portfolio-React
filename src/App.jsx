dd0e2896413d10dcc1c5e1e6dc25725a20640070
Sherbolot <arbaevsherbolot1@gmail.com>
2023-05-30 14:54:50 +0600

    Added loading...

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Wrapper } from "./components/wrapper/Wrapper";
 import "./App.scss";
 
 export const App = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 1700);
+  }, []);
+
+  // useEffect(() => {
+  //   setLoading(true);
+  //   window.onload = () => setLoading(false);
+  // }, []);
+
   return (
     <>
-      <Wrapper />
+      {loading ? (
+        <div
+          style={{
+            color: "#00ffbbeb",
+            height: "100vh",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            fontSize: "200px",
+            fontFamily: "PP Neue Machina, Ultrabold",
+          }}>
+          loading...
+        </div>
+      ) : (
+        <Wrapper />
+      )}
     </>
   );
 };
